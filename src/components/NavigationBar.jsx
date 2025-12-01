fad2d1cc5bbbac0c373d1ba3fe4044452cebf66c
Calify <kheijhackt3@gmail.com>
2025-12-01 16:35:12 +0800

    Fixed navbar style

--- src/components/NavigationBar.jsx
+++ src/components/NavigationBar.jsx
@@ -3,15 +3,33 @@ import { Link } from "react-router-dom";
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function NavigationBar() {
+  const navStyles = {
+    fontFamily: "'Nunito', sans-serif",
+    fontWeight: 700, // bold for navbar
+    fontSize: "1rem", // matches your earlier CSS base size
+  };
+
+  const brandStyles = {
+    fontFamily: "'Nunito', sans-serif",
+    fontWeight: 800,
+    fontSize: "1.3rem",
+  };
+
   return (
-    <Navbar bg="success" variant="dark" expand="lg" sticky="top">
+    <Navbar 
+      bg="success"
+      variant="dark"
+      expand="lg"
+      sticky="top"
+      style={{ fontFamily: "'Nunito', sans-serif" }}
+    >
       <Container>
-        <Navbar.Brand as={Link} to="/">Tonicist Association</Navbar.Brand>
+        <Navbar.Brand as={Link} to="/" style={brandStyles}>Tonicist Association</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ms-auto">
-            <Nav.Link as={Link} to="/recitals">Recitals</Nav.Link>
-            <Nav.Link as={Link} to="/about">About</Nav.Link>
+            <Nav.Link as={Link} to="/recitals" style={navStyles}>Recitals</Nav.Link>
+            <Nav.Link as={Link} to="/about" style={navStyles}>About</Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
