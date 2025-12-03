cffba8ab45f718843103c8bbb285555f7cec2de2
Calify <kheijhackt3@gmail.com>
2025-12-03 20:35:45 +0800

    Made the goal time for the event receive +8 gmt

--- src/components/CountdownTimer.jsx
+++ src/components/CountdownTimer.jsx
@@ -2,20 +2,31 @@ import Countdown from "react-countdown";
 
 // Reusable Countdown Component
 function CountdownTimer({ goalDate }) {
+  // Convert goalDate (assumed to be UTC+8) to actual UTC
+  const goalDateUTC = new Date(
+    new Date(goalDate).getTime() - 8 * 60 * 60 * 1000
+  );
+
   // Renderer function for custom display
   const renderer = ({ days, hours, minutes, seconds, completed }) => {
     if (completed) {
-      return <span style={{ color: "var(--green-dark)", fontWeight: 700 }}>The event has started!</span>;
+      return (
+        <span style={{ color: "var(--green-dark)", fontWeight: 700 }}>
+          The event has started!
+        </span>
+      );
     } else {
       return (
-        <div style={{
-          display: "flex",
-          gap: "15px",
-          fontFamily: "'Nunito', sans-serif",
-          fontWeight: 800,
-          color: "var(--green-dark)",
-          fontSize: "2rem",
-        }}>
+        <div
+          style={{
+            display: "flex",
+            gap: "15px",
+            fontFamily: "'Nunito', sans-serif",
+            fontWeight: 800,
+            color: "var(--green-dark)",
+            fontSize: "2rem",
+          }}
+        >
           <span>{days}d</span>
           <span>{hours}h</span>
           <span>{minutes}m</span>
@@ -25,9 +36,7 @@ function CountdownTimer({ goalDate }) {
     }
   };
 
-  return (
-    <Countdown date={new Date(goalDate)} renderer={renderer} />
-  );
+  return <Countdown date={goalDateUTC} renderer={renderer} />;
 }
 
 export default CountdownTimer;
