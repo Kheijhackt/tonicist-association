0cd1d5c23253370dec8a111a9935802544a11340
Calify <kheijhackt3@gmail.com>
2026-01-04 22:54:55 +0800

    Implemented visit counts

---
+++ api/update-visitCount.js
@@ -0,0 +1,28 @@
+export default async function handler(req, res) {
+  try {
+    if (req.method !== "GET") {
+      return res.status(405).json({ error: "Method not allowed" });
+    }
+
+    const url = process.env.UPDATE_VISIT_COUNT_API; // environment variable
+    if (!url) {
+      return res.status(500).json({ error: "API is not defined" });
+    }
+
+    const response = await fetch(url);
+
+    if (!response.ok) {
+      return res
+        .status(response.status)
+        .json({ error: "Failed to update visit count" });
+    }
+
+    const data = await response.json();
+    res.status(200).json(data);
+  } catch (err) {
+    res.status(500).json({
+      error: "Server error",
+      details: err.message,
+    });
+  }
+}
