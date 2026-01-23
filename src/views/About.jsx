c49b77551ff9998c0b486a3557542a458720629c
Calify <kheijhackt3@gmail.com>
2026-01-23 20:11:16 +0800

    Added a general description content on about page

--- src/views/About.jsx
+++ src/views/About.jsx
@@ -13,6 +13,20 @@ function About() {
         {contents.title}
       </h1>
 
+      {contents.description && (
+        <h5
+          style={{
+            textAlign: "center",
+            maxWidth: "700px",
+            margin: "0 auto 60px",
+            lineHeight: "1.6",
+            color: "var(--green-dark)",
+          }}
+        >
+          {contents.description}
+        </h5>
+      )}
+
       <div style={{ textAlign: "center" }}>
         {staffMembers.map((member, index) => (
           <AboutStaff
